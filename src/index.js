7199ce33b9649bb02af4f74f178ce3fd8c96baf8
Gerardo Torres <82909334+gerusadevops@users.noreply.github.com>
2023-08-08 20:41:10 -0600

    logs

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const xml2js = require("xml2js");
 
 app.use(cors());
 
@@ -10,9 +11,18 @@ app.get("/", (req, res) => {
     return res.send("Hola");
 });
 
-app.post("/evento-cms", bodyParser.text({type:"*/*"}) , (req, res) => {
+app.post("/evento-cms", bodyParser.text({type:"*/*"}) , async(req, res) => {
     console.log(req.headers);
-    console.log(req.body);
+
+    try {
+        const resultado = await xml2js.parseStringPromise(req.body);
+        console.log(resultado);
+        
+    } catch (error) {
+        console.log(error);
+    }
+
+
     return res.sendStatus(200);
 });
 
