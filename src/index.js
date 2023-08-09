40569574a00e4f9bf0b3e576e6c5f39aaa018272
Gerardo Torres <82909334+gerusadevops@users.noreply.github.com>
2023-08-08 18:09:04 -0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+
+
+app.post("evento-cms", (req, res) => {
+    console.log(req.body);
+    return res.sendStatus(200);
+});
+
+
+const port = 3000;
+app.listen(port, () => {
+    console.log("Servidor corriendo: " + port);
+});
